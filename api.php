83759a2db5962c8535be99b52e776dbcbc250d9c
hamzabdm <104500370+hamzabdm@users.noreply.github.com>
2025-04-28 20:55:01 +0200

    Update api.php

--- api.php
+++ api.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=exodia_clan', 'exodiaroot', 'exodia150root');
+$pdo = new PDO('mysql:host=localhost;dbname=exodia_clan', 'root', 'password');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $action = $_GET['action'] ?? '';
